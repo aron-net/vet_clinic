99136fe0e78f115fbddd8a828cd9c50cb5f1e21f
aron-helu <aronmilahela@gmail.com>
2022-09-22 10:13:37 +0300

    update

--- data.sql
+++ data.sql
@@ -38,8 +38,8 @@ VALUES  ('Pokemon'),
 
 UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
 UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
-UPDATE animals SET owners_id = 1 WHERE name = 'Agumon';
-UPDATE animals SET owners_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
-UPDATE animals SET owners_id = 3 WHERE name IN ('Devimon', 'Plantmon');
-UPDATE animals SET owners_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
-UPDATE animals SET owners_id = 5 WHERE name IN ('Angemon', 'Boarmon');
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
