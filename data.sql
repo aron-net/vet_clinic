f3ee561c781488442b8f72b83de23c78a391a560
aron-helu <aronmilahela@gmail.com>
2022-09-22 10:00:25 +0300

    add species data and update the table

--- data.sql
+++ data.sql
@@ -32,3 +32,14 @@ VALUES  ('Sam Smith', 34),
         ('Dean Winchester', 14),
         ('Jodie Whittaker', 38);
 
+INSERT INTO species (name)
+VALUES  ('Pokemon'),
+        ('Digimon');
+
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
+UPDATE animals SET owners_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owners_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owners_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owners_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owners_id = 5 WHERE name IN ('Angemon', 'Boarmon');
